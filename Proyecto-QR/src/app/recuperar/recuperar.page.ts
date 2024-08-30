ea002981f7a8be62dc9107d5142073347c150dbf
Dashaketa <164213023+Dashaketa@users.noreply.github.com>
2024-08-29 21:39:50 -0400

    Skeleton

---
+++ Proyecto-QR/src/app/recuperar/recuperar.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-recuperar',
+  templateUrl: './recuperar.page.html',
+  styleUrls: ['./recuperar.page.scss'],
+})
+export class RecuperarPage implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  recuperarPass(){
+    this.router.navigate(['/home'])
+  }
+
+}
